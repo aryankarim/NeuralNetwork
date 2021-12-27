98b989ee99e4234646d126931d91bb19d645db30
aryanmajeed <aryanmajid97@gmail.com>
2021-12-27 06:49:02 +0300

    build: test setup

---
+++ build/CMakeFiles/neuralnetwork.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/neuralnetwork.dir/main.cpp.o"
+  "CMakeFiles/neuralnetwork.dir/main.cpp.o.d"
+  "neuralnetwork"
+  "neuralnetwork.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/neuralnetwork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
